4f87b09d912e2f9a3b6054e0d309936529a48e3a
comSaaxov <comradesaaxov@gmail.com>
2015-07-29 23:15:28 +0300

    psd
    new psd

---
+++ One page/js/common.js
@@ -0,0 +1,107 @@
+
+$(document).ready(function() {
+$("#portfolio_grid").mixItUp();
+$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+	$("a[rel^='prettyPhoto']").prettyPhoto();
+
+	$(function() {
+		$('.banner').unslider({
+				fluid: true,
+				dots: true,
+				pause: true
+			});
+			
+			
+		    var unslider = $('.banner').unslider();
+		    $('.unslider-arrow').click(function() {
+		        var fn = this.className.split(' ')[1];
+		        unslider.data('unslider')[fn]();
+		    });
+	});
+
+
+
+	//Таймер обратного отсчета
+	//Документация: http://keith-wood.name/countdown.html
+	//<div class="countdown" date-time="2015-01-07"></div>
+	var austDay = new Date($(".countdown").attr("date-time"));
+	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
+
+	//Попап менеджер FancyBox
+	//Документация: http://fancybox.net/howto
+	//<a class="fancybox"><img src="image.jpg" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	$(".fancybox").fancybox();
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	$(".top_mnu").navigation();
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Каруселька
+	//Документация: http://owlgraphic.com/owlcarousel/
+	$("#owl-demo").owlCarousel({
+ 
+      //navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
